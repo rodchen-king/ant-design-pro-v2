248a4fb61e828fb83f08eb8ab71f8e7dd7e0dc39
rodchen_king <rodchen_king@outlook.com>
2020-08-31 21:39:40 +0800

    1. 删除多余代码

--- config/router.config.js
+++ config/router.config.js
@@ -18,29 +18,7 @@ export default [
     authority: ["admin", "user"],
     routes: [
       // dashboard
-      { path: "/", redirect: "/dashboard/analysis" },
-      {
-        path: "/dashboard",
-        name: "dashboard",
-        icon: "dashboard",
-        routes: [
-          {
-            path: "/dashboard/analysis",
-            name: "analysis",
-            component: "./Dashboard/Analysis"
-          },
-          {
-            path: "/dashboard/monitor",
-            name: "monitor",
-            component: "./Dashboard/Monitor"
-          },
-          {
-            path: "/dashboard/workplace",
-            name: "workplace",
-            component: "./Dashboard/Workplace"
-          }
-        ]
-      },
+      { path: "/", redirect: "/list/table-list" },
       // forms
       {
         path: "/form",
@@ -51,39 +29,6 @@ export default [
             path: "/form/basic-form",
             name: "basicform",
             component: "./Forms/BasicForm"
-          },
-          {
-            path: "/form/step-form",
-            name: "stepform",
-            component: "./Forms/StepForm",
-            hideChildrenInMenu: true,
-            routes: [
-              {
-                path: "/form/step-form",
-                redirect: "/form/step-form/info"
-              },
-              {
-                path: "/form/step-form/info",
-                name: "info",
-                component: "./Forms/StepForm/Step1"
-              },
-              {
-                path: "/form/step-form/confirm",
-                name: "confirm",
-                component: "./Forms/StepForm/Step2"
-              },
-              {
-                path: "/form/step-form/result",
-                name: "result",
-                component: "./Forms/StepForm/Step3"
-              }
-            ]
-          },
-          {
-            path: "/form/advanced-form",
-            name: "advancedform",
-            authority: ["admin"],
-            component: "./Forms/AdvancedForm"
           }
         ]
       },
@@ -97,42 +42,6 @@ export default [
             path: "/list/table-list",
             name: "searchtable",
             component: "./List/TableList"
-          },
-          {
-            path: "/list/basic-list",
-            name: "basiclist",
-            component: "./List/BasicList"
-          },
-          {
-            path: "/list/card-list",
-            name: "cardlist",
-            component: "./List/CardList"
-          },
-          {
-            path: "/list/search",
-            name: "searchlist",
-            component: "./List/List",
-            routes: [
-              {
-                path: "/list/search",
-                redirect: "/list/search/articles"
-              },
-              {
-                path: "/list/search/articles",
-                name: "articles",
-                component: "./List/Articles"
-              },
-              {
-                path: "/list/search/projects",
-                name: "projects",
-                component: "./List/Projects"
-              },
-              {
-                path: "/list/search/applications",
-                name: "applications",
-                component: "./List/Applications"
-              }
-            ]
           }
         ]
       },
@@ -156,22 +65,9 @@ export default [
         ]
       },
       {
-        name: "result",
-        icon: "check-circle-o",
-        path: "/result",
-        routes: [
-          // result
-          {
-            path: "/result/success",
-            name: "success",
-            component: "./Result/Success"
-          },
-          { path: "/result/fail", name: "fail", component: "./Result/Error" }
-        ]
-      },
-      {
         name: "exception",
         icon: "warning",
+        hideInMenu: true,
         path: "/exception",
         routes: [
           // exception
@@ -195,63 +91,6 @@ export default [
             name: "trigger",
             hideInMenu: true,
             component: "./Exception/TriggerException"
-          }
-        ]
-      },
-      {
-        name: "account",
-        icon: "user",
-        path: "/account",
-        routes: [
-          {
-            path: "/account/center",
-            name: "center",
-            component: "./Account/Center/Center",
-            routes: [
-              {
-                path: "/account/center",
-                redirect: "/account/center/articles"
-              },
-              {
-                path: "/account/center/articles",
-                component: "./Account/Center/Articles"
-              },
-              {
-                path: "/account/center/applications",
-                component: "./Account/Center/Applications"
-              },
-              {
-                path: "/account/center/projects",
-                component: "./Account/Center/Projects"
-              }
-            ]
-          },
-          {
-            path: "/account/settings",
-            name: "settings",
-            component: "./Account/Settings/Info",
-            routes: [
-              {
-                path: "/account/settings",
-                redirect: "/account/settings/base"
-              },
-              {
-                path: "/account/settings/base",
-                component: "./Account/Settings/BaseView"
-              },
-              {
-                path: "/account/settings/security",
-                component: "./Account/Settings/SecurityView"
-              },
-              {
-                path: "/account/settings/binding",
-                component: "./Account/Settings/BindingView"
-              },
-              {
-                path: "/account/settings/notification",
-                component: "./Account/Settings/NotificationView"
-              }
-            ]
           }
         ]
       },
