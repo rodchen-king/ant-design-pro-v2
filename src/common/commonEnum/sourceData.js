d6de1f9f692735097b6c51b36a1d7cfe01ea4bf6
rodchen_king <rodchen_king@outlook.com>
2020-09-01 21:12:03 +0800

    1. 添加枚举类型值

---
+++ src/common/commonEnum/sourceData.js
@@ -0,0 +1,16 @@
+const sourceData = {
+  userAccountTypeEnum: [
+    {
+      key: "STAFF",
+      value: 1,
+      label: "员工"
+    },
+    {
+      key: "COMPANY",
+      value: 2,
+      label: "公司"
+    }
+  ]
+};
+
+export default sourceData;
